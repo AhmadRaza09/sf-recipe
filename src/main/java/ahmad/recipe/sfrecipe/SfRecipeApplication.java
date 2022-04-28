45b7066cad473f42fe98ca938b041acfed03804e
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-04-28 12:15:18 +0500

    first commit

---
+++ src/main/java/ahmad/recipe/sfrecipe/SfRecipeApplication.java
@@ -0,0 +1,13 @@
+package ahmad.recipe.sfrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfRecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ahmad/recipe/sfrecipe/SfRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package ahmad.recipe.sfrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
