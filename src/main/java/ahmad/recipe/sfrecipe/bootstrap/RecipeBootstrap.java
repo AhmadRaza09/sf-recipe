95fa9262ae8eb18384a6452bc50f6e4c3a031c3d
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-05-12 12:29:14 +0500

    refactored code by using lombok. closes #8.

--- src/main/java/ahmad/recipe/sfrecipe/bootstrap/RecipeBootstrap.java
+++ src/main/java/ahmad/recipe/sfrecipe/bootstrap/RecipeBootstrap.java
@@ -9,15 +9,18 @@ import ahmad.recipe.sfrecipe.models.UnitOfMeasure;
 import ahmad.recipe.sfrecipe.repostories.CategoryRepository;
 import ahmad.recipe.sfrecipe.repostories.RecipeRepository;
 import ahmad.recipe.sfrecipe.repostories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -34,8 +37,10 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 
 
     @Override
+    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
         recipeRepository.saveAll(getRecipes());
+        log.debug("Loading Bootstrap Data");
     }
 
     private List<Recipe> getRecipes()
--- src/main/java/ahmad/recipe/sfrecipe/controllers/IndexController.java
+++ src/main/java/ahmad/recipe/sfrecipe/controllers/IndexController.java
@@ -1,11 +1,13 @@
 package ahmad.recipe.sfrecipe.controllers;
 
 import ahmad.recipe.sfrecipe.service.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -18,6 +20,7 @@ public class IndexController {
     @RequestMapping(value ={"", "/", "/index", "/index.html"} , method = RequestMethod.GET)
      public String getIndexPage(Model model) {
 
+        log.debug("Getting Index page.");
 //        set title
         model.addAttribute("title", "Recipe Home");
 
