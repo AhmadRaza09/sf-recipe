33111337a811076c255748721c57469f04ddfff2
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-05-13 11:40:16 +0500

    refacatored indexController test

--- src/test/java/ahmad/recipe/sfrecipe/controllers/IndexControllerTest.java
+++ src/test/java/ahmad/recipe/sfrecipe/controllers/IndexControllerTest.java
@@ -1,18 +1,21 @@
 package ahmad.recipe.sfrecipe.controllers;
 
 
+import ahmad.recipe.sfrecipe.models.Recipe;
 import ahmad.recipe.sfrecipe.service.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 
 class IndexControllerTest {
@@ -34,12 +37,31 @@ class IndexControllerTest {
     @Test
     void getIndexPage() {
 
+//        given
+        Set<Recipe> recipes = new HashSet<>();
+
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(2L);
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor =  ArgumentCaptor.forClass(Set.class);
+
+//        when
         String viewName = indexController.getIndexPage(model);
 
+//        then
         assertEquals(viewName, "index");
 
         verify(recipeService, times(1)).getRecipes();
         verify(model, times(1)).addAttribute(eq("title"), eq("Recipe Home"));
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+
+        Set<Recipe> setIndexController = argumentCaptor.getValue();
+
+        assertEquals(2, setIndexController.size());
     }
 }
