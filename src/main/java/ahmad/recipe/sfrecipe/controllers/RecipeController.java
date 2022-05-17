050105b6c12b71a544b482bcca99fbfdfd3b26bd
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-05-17 21:58:13 +0500

    added functionality to create new recipe. closes #17

--- src/main/java/ahmad/recipe/sfrecipe/controllers/RecipeController.java
+++ src/main/java/ahmad/recipe/sfrecipe/controllers/RecipeController.java
@@ -1,8 +1,10 @@
 package ahmad.recipe.sfrecipe.controllers;
 
+import ahmad.recipe.sfrecipe.commands.RecipeCommand;
 import ahmad.recipe.sfrecipe.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,6 +25,22 @@ public class RecipeController {
 
         model.addAttribute("recipe", recipeService.findById(Long.parseLong(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping(value = {"/recipe/new"}, method = RequestMethod.GET)
+    public String newRecipe(Model model)
+    {
+        model.addAttribute("title", "Create Recipe");
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @RequestMapping(value = {"/recipe"},method = RequestMethod.POST)
+    public String savedOrUpdate(@ModelAttribute(value = "recipe") RecipeCommand recipeCommand)
+    {
+        RecipeCommand savedRecipe = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + savedRecipe.getId();
     }
 
 }
