8c16277c2510bcd72be74e6ae2064e845c290612
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-06-11 14:29:00 +0500

    added image in db. closes #26

--- src/main/java/ahmad/recipe/sfrecipe/service/ImageServiceImpl.java
+++ src/main/java/ahmad/recipe/sfrecipe/service/ImageServiceImpl.java
@@ -1,14 +1,46 @@
 package ahmad.recipe.sfrecipe.service;
 
+import ahmad.recipe.sfrecipe.models.Recipe;
+import ahmad.recipe.sfrecipe.repostories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.transaction.Transactional;
+import java.io.IOException;
+
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
+    @Transactional
     public void saveImageFile(Long id, MultipartFile file) {
         log.debug("Image Uploaded");
+        try{
+            Recipe recipe = recipeRepository.findById(id).get();
+
+            Byte [] bytesObject = new Byte[file.getBytes().length];
+
+            int i = 0;
+            for(byte b : file.getBytes()){
+                bytesObject[i++] = b;
+            }
+
+            recipe.setImage(bytesObject);
+
+            recipeRepository.save(recipe);
+        }
+        catch(IOException e){
+            //todo handle better
+            log.error("Error occured: " + e);
+            e.printStackTrace();
+        }
     }
+
 }
